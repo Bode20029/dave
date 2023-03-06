84e8d86ba1ad627c89c81e422d839345ea3f534c
Bode <jry.bode@gmail.com>
2023-03-06 21:14:02 +0700

    i

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ const handleDelete = (id) => {
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
-      <Footer/>
+      <Footer length={items.length}  />
     </div>
   );
 }
